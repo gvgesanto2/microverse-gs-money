8f9bbe6759c206d71300569edcba9aa7617fec84
Gabriel Santo <gvgesanto2@gmail.com>
2022-10-27 09:45:17 -0300

    feat: Set up Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  # SMTP settings for mailcatcher
+  config.action_mailer.smtp_settings = {
+    address: '127.0.0.1', 
+    port: 1025
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
