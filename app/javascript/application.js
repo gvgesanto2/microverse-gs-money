995034d9059a6634eb21487ae05e52b0a762c86a
Gabriel Santo <gvgesanto2@gmail.com>
2022-10-30 15:35:32 -0300

    feat: Add the setup-scroll.js script's code to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,32 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./setup-scroll"
+// import "./setup-scroll"
+
+import ScrollBooster from "scrollbooster";
+
+const setupScrolls = () => {
+  const viewports = document.querySelectorAll(".u-scroll-viewport");
+  const contents = document.querySelectorAll(".u-scroll-content");
+
+  viewports.forEach((viewport, index) => {
+    new ScrollBooster({
+      viewport,
+      content: contents[index],
+      direction: "horizontal",
+      scrollMode: "native",
+    });
+  });
+}
+
+function init() {
+  setupScrolls();
+} 
+
+document.addEventListener("DOMSubtreeModified", init);
+
+if (document.readyState !== "loading") {
+  init();
+} else {
+  document.addEventListener("DOMContentLoaded", init);
+}
