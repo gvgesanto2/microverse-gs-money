33105aec6ff70412904d4cce20dc71024152f75e
Gabriel Santo <gvgesanto2@gmail.com>
2022-10-30 23:14:41 -0300

    refactor: Change styles to use margin instead of the gap property

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -30,17 +30,3 @@ if (document.readyState !== "loading") {
 } else {
   document.addEventListener("DOMContentLoaded", init);
 }
-
-export const addClassToElem = (htmlElem, classToAdd) => {
-  htmlElem.classList.add(classToAdd);
-}
-
-export const toggleClassFromElem = (htmlElem, classToAdd) => {
-  htmlElem.classList.toggle(classToAdd);
-}
-
-export const removeClassFromElem = (htmlElem, classToAdd) => {
-  if (htmlElem.classList.contains(classToAdd)) {
-    htmlElem.classList.remove(classToAdd);
-  }
-}
