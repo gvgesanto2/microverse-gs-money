2f26a38c79697140e4dfda4f802884b1ad8c6e68
Gabriel Santo <gvgesanto2@gmail.com>
2022-11-29 18:09:25 -0300

    feat: Add a back button to the new transaction page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :email, :password, :current_password, :image)
     end
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
